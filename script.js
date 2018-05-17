a0ba619808fbd4173f1b16662306c98adf8edc7c
Júlio Campos <julio.campos@uniriotec.br>
2018-05-17 12:48:52 -0300

    Redesenho das funções
    Implementação inicial do endpoint de comentários

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// Object
-var obj = {
-  pullRequests: [],
-  comments : []
-}
-
-var pullRequestsNumbers = [];
-
-// Clean
-function clean() {
-  obj = {
-    pullRequests: []
-  };
-
-  pullRequestsNumbers = [];
-  lastCursor = '';
-}
-
-// Execute clean
-clean();
-
 // Requires
 var fetch = require('node-fetch');
 var fs = require('fs');
@@ -30,15 +9,27 @@ var fs = require('fs');
 */
 var accessToken = '';
 
+// Object
+var obj = {
+  pullRequests: [],
+  comments : []
+}
+
 // Variables
 var user = 'github';
 var repository = 'scientist';
 var amount = 1;
-var lastCursor = null;
-var query = `
-query github ($user: String!, $repository: String!, $lastCursor: String, $amount: Int!) {
+var lastCursorPulls = null;
+var lastCursorComments = null;
+var hasNextPage = null;
+var pullRequestsNumbers = [];
+var number = 79;
+
+// Queries
+var queryPullRequets = `
+query pullRequests ($user: String!, $repository: String!, $lastCursorPulls: String, $amount: Int!) {
   repository(owner: $user, name: $repository) {
-    pullRequests(first: $amount, after: $lastCursor, states: CLOSED) {
+    pullRequests(first: $amount, after: $lastCursorPulls, states: CLOSED) {
       totalCount
       edges {
         node {
@@ -85,16 +76,52 @@ query github ($user: String!, $repository: String!, $lastCursor: String, $amount
 }
 `;
 
+var queryComments = `
+query comments ($user: String!, $repository: String!, $lastCursorComments: String, $number: Int!, $amount: Int!) {
+  repository(owner: $user, name: $repository) {
+    pullRequest(number: $number) {
+      number
+      comments(first: $amount, after: $lastCursorComments) {
+        totalCount
+        edges {
+          node {
+            author {
+              login
+            }
+            authorAssociation
+            body
+            createdAt
+            editor {
+              login
+            }
+            id
+            lastEditedAt
+            publishedAt
+            updatedAt
+            viewerDidAuthor
+          }
+          cursor
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+      }
+    }
+  }
+}
+`;
+
 function extractPullRequests() {
 
   fetch('https://api.github.com/graphql', {
     method: 'POST',
     body: JSON.stringify({
-      query: query, 
+      query: queryPullRequets, 
       variables: {
         user: user,
         repository: repository,
-        lastCursor: lastCursor,
+        lastCursorPulls: lastCursorPulls,
         amount: amount
       },
     }),
@@ -104,35 +131,94 @@ function extractPullRequests() {
   }).then(res => res.json())
     .then(body => savePullRequests(body))
     .then(data => fs.writeFile( user + '_' + repository + '.json', JSON.stringify(obj, null, '  '), callback))
-    .then(next => extractPullRequests(next))
+    .then(next => {
+        if (hasNextPage) {
+          extractPullRequests(next);
+        } else {
+          extractComments();
+        }
+      }
+    )
     .catch(error => console.error(error));
 }
+
 extractPullRequests();
 
 function savePullRequests(body) {
 
-  for(var i = 0; i < amount; i++) {
+  hasNextPage = body.data.repository.pullRequests.pageInfo.hasNextPage;
 
-    var item = body.data.repository.pullRequests.edges[i].node;
-    obj.pullRequests.push(item);
+  if(hasNextPage) {
+    for(var i = 0; i < amount; i++) {
 
-    var pullRequestNumber = body.data.repository.pullRequests.edges[i].node.number;
-    pullRequestsNumbers.push(pullRequestNumber);
+      var item = body.data.repository.pullRequests.edges[i].node;
+      obj.pullRequests.push(item);
+      // console.log(item);
 
-    lastCursor = body.data.repository.pullRequests.pageInfo.endCursor;
-    var hasNextPage = body.data.repository.pullRequests.pageInfo.hasNextPage;
-  }
+      var pullRequestNumber = body.data.repository.pullRequests.edges[i].node.number;
+      pullRequestsNumbers.push(pullRequestNumber);
 
-  console.log("\n" + obj.pullRequests.length + " pull requests");
-  console.log("Cursor: " + lastCursor);
-
-  if (hasNextPage == true) {
-      return body;
-  } else {
-      return console.log("Finished!");
+      lastCursorPulls = body.data.repository.pullRequests.pageInfo.endCursor;
+    }
+      console.log("\n" + obj.pullRequests.length + " pull requests");
+      console.log("Cursor: " + lastCursorPulls);
   }
 }
 
+function extractComments() {
+
+  fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    body: JSON.stringify({
+      query: queryComments, 
+      variables: {
+        user: user,
+        number: number,
+        repository: repository,
+        lastCursorComments: lastCursorComments,
+        amount: amount
+      },
+    }),
+    headers: {
+      'Authorization': `Bearer ${accessToken}`,
+    },
+  }).then(res => res.json())
+    .then(body => saveComments(body))
+    .then(data => fs.writeFile( user + '_' + repository + '.json', JSON.stringify(obj, null, '  '), callback))
+    .then(next => {
+        if (hasNextPage) {
+          extractComments(next);
+        } else {
+          console.log("Finished!");
+        }
+      }
+    )
+    .catch(error => console.error(error));
+}
+
+function saveComments(body) {
+
+  hasNextPage = body.data.repository.pullRequest.comments.pageInfo.hasNextPage;
+
+  if (hasNextPage) {
+    for(var i = 0; i < amount; i++) {
+
+      var number = body.data.repository.pullRequest.number;
+      var item = body.data.repository.pullRequest.comments.edges[i].node;
+      obj.comments.push({number: number, item: item});
+
+      lastCursorComments = body.data.repository.pullRequest.comments.pageInfo.endCursor;
+    }
+
+    console.log("\n" + obj.comments.length + " comments");
+    console.log("Cursor: " + lastCursorComments);
+  }
+
+}
+
 function callback(status) {
-  console.log('Saved data!');
+
+  if (hasNextPage) {
+    console.log('Saved data!');
+  }
 }
